5041a0b714fe715f264ca5ad9d30d23693dfe9cb
karin valcanover <valcanoverkarin@outlook.it>
2021-03-24 15:26:56 +0100

    chiedo cognome piu lista cognomi

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 //2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 //3. stampa la lista ordinata alfabeticamente
 //4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova (facciamo questo in 2 versioni: col ciclo for e col while)
+
+//chiedo all’utente il cognome
+var cognomeUtente = prompt ('Inserisci il tuo cognome');
+console.log(cognomeUtente);
+
+//array con altri cognomi
+var arrayCognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+//console.log(arrayCognomi);
